7472602f0a762ffea4199535f39b3fbefb41c52f
Mudassirraza912 <39431881+Mudassirraza912@users.noreply.github.com>
2018-11-11 23:02:40 +0500

    Update dashboard.js

--- MeetUp App Task/src/components/dashboard/dashboard.js
+++ MeetUp App Task/src/components/dashboard/dashboard.js
@@ -1,3 +1,7 @@
+
+/* eslint-disable no-undef */
+/* global google */
+
 import React, { Component } from 'react';
 import Cards, { Card } from 'react-swipe-deck'
 import firebase from '../config/firebase'
@@ -5,12 +9,13 @@ import firebase from '../config/firebase'
 // import img3 from '../images/img2.jpg'; 
 // import img2 from '../images/img3.jpg'; 
 
-// import './app.css' 
+import '../../App.css'
 import { Slide } from 'react-slideshow-image';
 import swal from 'sweetalert'
-import { Button, TextField,Radio } from '@material-ui/core'
-
-
+import {Avatar, Button, TextField, Radio, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, ListItem, ListItemText } from '@material-ui/core'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, DirectionsRenderer } from "react-google-maps"
+// import{ ExpandMoreIcon } from '@material-ui/icons'
+import PropReq from './propsal&Req'
 
 
 // const data = [ {image:"img1", name:'Haris Ansari' , nickName:'Haris'}, {image:"img2", name:'Muneeb Ansari' , nickName:'Muneeb'}, {image:"img1", name:'Muhammad Umar' , nickName:'Umar'}]; 
@@ -41,9 +46,17 @@ class Dashboard extends Component {
             searchTerm: false,
             searchLocation: false,
             selectLocation: '',
-            calender:'',
-            selectDate:'',
-            alldone:false
+            calender: '',
+            selectDate: '',
+            alldone: false,
+            locationRender: false,
+            directions: '',
+            recieverUid: '',
+            proposals: [],
+            userData: '',
+            recieverName:'',
+            recieverPhoto:'',
+            propSend:[]
         }
         this.actionLeft = this.actionLeft.bind(this)
         this.setMet = this.setMet.bind(this)
@@ -52,32 +65,62 @@ class Dashboard extends Component {
         this.showlocations = this.showlocations.bind(this)
         this.renderLocations = this.renderLocations.bind(this)
         this.serachLoc = this.serachLoc.bind(this)
-        // this.setLocation = this.setLocation.bind(this)
+        this.renderDirection = this.renderDirection.bind(this)
+        this.getDirections = this.getDirections.bind(this)
+        this.sendProposal = this.sendProposal.bind(this)
 
     }
 
     componentWillMount() {
-        const { data, slideImages, direction } = this.state
-        firebase.database().ref('Users/' + firebase.auth().currentUser.uid + '/').on('child_added', snap => {
-            console.log(snap.val());
+        const { data, slideImages, direction, proposals, userData, propSend } = this.state
+        firebase.database().ref('/Users/').on('child_added', snap => {
+            // console.log(snap.val(),snAp.url,imagerUrl);
             var snAp = snap.val()
             console.log(snAp.beverages);
             var latlng = snAp.direction
             var dish = snAp.beverages
-            data.push({ nickName: snAp.nickName, name: firebase.auth().currentUser.displayName });
+            var imagerUrl = snAp.url
+            data.push({ nickName: snAp.nickName, name: snAp.profileName, url: snAp.url, uid: snAp.uid, profileUrl : snAp.profileUrl });
+            console.log(snap.val(), snAp.url, data);
+
             slideImages.push(...snAp.url)
             this.setState({
                 data,
                 slideImages,
                 direction: latlng,
-                beverages: dish
+                beverages: dish,
+
 
             })
         })
-        // venues/explore?client_id=ZJWQ050MTAJAQVAU2GHBN2WI3EIJJ1ZKNNBWTF1CYLVKKRA0&client_secret=NNAT41FP4ZLRDDE2QPNVMNAS4GFKICS5R0Z2DQ4MKFEW1SVT&v=20180323&ll=24.958101%2C-66.9854055&query=park
-        // fetch('https://api.foursquare.com/v2/venues/explore?client_id=ZJWQ050MTAJAQVAU2GHBN2WI3EIJJ1ZKNNBWTF1CYLVKKRA0&client_secret=NNAT41FP4ZLRDDE2QPNVMNAS4GFKICS5R0Z2DQ4MKFEW1SVT&v=20180323&ll=24.958101,66.9854055&query=park&limit=3')
-        //   .then(response => response.json())
-        //   .then(data => console.log(data.response.groups[0].items));
+
+        firebase.database().ref('/' + firebase.auth().currentUser.uid + '/myData/').on('child_added', snap => {
+            console.log(snap.val())
+            this.setState({
+                userData: snap.val()
+            })
+        })
+
+        firebase.database().ref('/' + firebase.auth().currentUser.uid + '/proposalRequest/').on('child_added', (propsalSnap) => {
+            var propSnap = propsalSnap.val()
+
+            proposals.push(propSnap)
+
+            this.setState({
+                proposals,
+            })
+        })
+
+
+        firebase.database().ref('/' + firebase.auth().currentUser.uid + '/proposalSend/').on('child_added', (propsalSnap) => {
+            var propSnap = propsalSnap.val()
+
+            propSend.push(propSnap)
+
+            this.setState({
+                propSend,
+            })
+        })
 
     }
 
@@ -116,32 +159,116 @@ class Dashboard extends Component {
     setLocation(location) {
         console.log(location)
         this.setState({
-           calender:true,
-           selectLocation:location
+            //    calender:true,
+            selectLocation: location,
+            locationRender: true,
+            location: false,
         })
     }
 
-    renderCalender(){
-        return(
+
+    renderDirection() {
+        const { selectLocation, directions, direction } = this.state
+        return (
             <div>
-                <TextField
-        id="datetime-local"
-        label="Next appointment"
-        type="datetime-local"
-        defaultValue="2017-05-24T10:30"
-        onChange={(e) => {this.setState({
-            selectDate:e.target.value
-        })
-        }}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-      <Button color='primary' onClick={() => {this.setState({alldone:true, setMet:false,calender:false,location:false}), swal("your request send", {
-                        icon: "success",
-                    });}}>Send</Button>
+                <MyMapComponent
+                    isMarkerShown
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                    cord={selectLocation.location}
+                    destination={direction}
+                    new={this.newPosition}
+                    directions={directions}
+                // defaultDraggable = {true} 
+                /><br /><br /><br />
+
+                <Button onClick={this.getDirections}>Get Direction</Button><Button onClick={() => { this.setState({ calender: true, locationRender: false }) }}>Next</Button>
+
             </div>
         )
+
+    }
+
+
+    getDirections() {
+
+        const { selectLocation, direction, } = this.state
+        const DirectionsService = new google.maps.DirectionsService();
+
+        DirectionsService.route({
+            origin: new google.maps.LatLng(direction.latitude, direction.loongitude),
+            destination: new google.maps.LatLng(selectLocation.location.lat, selectLocation.location.lng),
+            travelMode: google.maps.TravelMode.DRIVING,
+        }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+                this.setState({
+                    directions: result,
+                });
+            } else {
+                alert("Sorry! Can't calculate directions!")
+            }
+        });
+    }
+
+
+    renderCalender() {
+        const {selectDate} = this.state
+        return (
+            <div>
+                <TextField
+                    id="datetime-local"
+                    label="Next appointment"
+                    type="datetime-local"
+                    defaultValue="2017-05-24T10:30"
+                    onChange={(e) => {
+                        this.setState({
+                            selectDate: e.target.value
+                        })
+                    }}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+             {!selectDate ?  <Button disabled color='primary' onClick={this.sendProposal}>Send</Button> : 
+            <Button  color='primary' onClick={this.sendProposal}>Send</Button>}
+            </div>
+        )
+    }
+
+    sendProposal() {
+        const { data, slideImages, beverages, selectDate, selectLocation, recieverUid, userData, recieverName, recieverPhoto } = this.state
+        console.log(data, slideImages, beverages, selectDate, selectLocation, recieverUid, )
+        var proposalObj = {
+            senderData: userData,
+            senderImages: slideImages,
+            beverages: beverages,
+            endTime: selectDate,
+            startTime: `${new Date().toDateString()}T${new Date().toLocaleTimeString()}`,
+            selectLocation: selectLocation,
+            senderUid: firebase.auth().currentUser.uid,
+            recieverUid: recieverUid,
+            users: data,
+            status: 'Pending',
+            recieverName:recieverName,
+            recieverPhoto: recieverPhoto
+        }
+
+        // firebase.database().ref('/' + firebase.auth().currentUser.uid + '/proposalSend/').push(proposalObj)
+        // firebase.database().ref('/' + recieverUid + '/proposalRequest/').push(proposalObj)
+        firebase.database().ref('/Proposals/').push(proposalObj)
+
+        this.setState({
+            alldone: true,
+            setMet: false,
+            calender: false,
+            location: false
+        }),
+            swal("your request send",
+                {
+                    icon: "success",
+                });
     }
 
     renderLocations() {
@@ -187,10 +314,14 @@ class Dashboard extends Component {
             data,
         })
     }
-    actionRight(e, name) {
-        console.log(e)
+    actionRight(e, name, uid, photo) {
+        const {recieverName, recieverPhoto} = this.state
+        console.log(e, uid)
         this.setState({
-            setMet: false
+            setMet: false,
+            recieverUid: uid,
+            recieverName:name,
+            recieverPhoto: photo
         })
         swal({
             title: "Are you sure?",
@@ -224,52 +355,62 @@ class Dashboard extends Component {
         })
     }
     render() {
-        const { setMet, data, slideImages, location,calender,selectDate,selectLocation,beverages} = this.state
-        console.log(slideImages, selectDate,selectLocation,beverages)
+        const { setMet, data, slideImages, location, calender, selectDate, selectLocation, beverages, locationRender, direction,proposals, propSend } = this.state
+        console.log('IMAGE URL', slideImages, 'DATE', selectDate, 'SELECT LOCATIOM', selectLocation, 'BEVEREGES', beverages, 'MY DIRECTION', direction)
         return (
 
             <div>
                 <center>
                     {!calender && !setMet && <div>
+
+                        {proposals.length == 0 || proposals.length == 0 &&
                         <h2>You haven’t done any meeting yet! <br /> try creating a new meeting! </h2>
+                        }
+                         
+
                         <Button color='primary' onClick={this.setMet}> Set a meeting</Button>
                     </div>
                     }
 
-                    { !calender && !location && setMet && <div className='card'>
+                    {!locationRender && !calender && !location && setMet && <div className='card'>
                         <Cards onEnd={() => this.action('function end')} className='master-root'>
-                            {data.map((item, index) =>
-                                <Card
-                                    onSwipeLeft={() => this.actionLeft('swipe left', index)}
-                                    onSwipeRight={() => this.actionRight('swipe right', item.name)}>
-                                    <div>
-                                        <Slide {...properties}>
-                                            <div className="each-slide">
-                                                <div style={{ 'backgroundImage': `url(${slideImages[0]})`, height: '100px' }}>
-                                                    <span>Slide 1</span>
+                            {data.map((item, index) => {
+                                console.log(item.uid, item)
+                                return (
+                                    <Card
+                                        onSwipeLeft={() => console.log('swipe left')}
+                                        onSwipeRight={() => this.actionRight('swipe right', item.name, item.uid, item.profileUrl)} 
+                                        >
+                                        <div>
+                                            <Slide {...properties}>
+                                                <div className="each-slide">
+                                                    <div style={{ 'backgroundImage': `url(${item.url[0]})`, height: '320px', width: '320px', backgroundRepeat: 'none', backgroundSize: 'cover', backgroundColor: 'gray' }}>
+                                                        <span>Picture 1</span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div className="each-slide">
-                                                <div style={{ 'backgroundImage': `url(${slideImages[1]})`, height: '100px' }}>
-                                                    <span>Slide 2</span>
+                                                <div className="each-slide">
+                                                    <div style={{ 'backgroundImage': `url(${item.url[1]})`, height: '320px', width: '320px', backgroundRepeat: 'none', backgroundSize: 'cover', backgroundColor: 'gray'  }}>
+                                                        <span>Picture 2</span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div className="each-slide">
-                                                <div style={{ 'backgroundImage': `url(${slideImages[2]})`, height: '100px' }}>
-                                                    <span>Slide 3</span>
+                                                <div className="each-slide">
+                                                    <div style={{ 'backgroundImage': `url(${item.url[2]})`, height: '320px', width: '320px', backgroundRepeat: 'none', backgroundSize: 'cover', backgroundColor: 'gray'  }}>
+                                                        <span>Picture 3</span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </Slide>
-                                        <div className='list'>
-                                            <center><h3>{item.name}</h3></center>
-                                            <center><b>{item.nickName}</b></center>
-                                            <div className='accRej'>
-                                                <Button color='primary' className='acc' onClick={() => this.actionRight('swipe right', item.name)}> <img src='https://www.healthandsafetysigns.co.uk/wp-content/uploads/2017/08/tick.png' width='55px' height='40px' /></Button>
-                                                <Button color='primary' className='Rej' onClick={() => this.actionLeft('swipe left', index)}> <img src='http://www.urltarget.com/images/cancel-delete-cross-check-box-check-tick-icon.png' width='55px' height='40px' /></Button>
+                                            </Slide>
+                                            <div className='list'>
+                                                <center><h3>{item.name}</h3></center>
+                                                <center><b>{item.nickName}</b></center>
+                                                <div className='accRej'>
+                                                    <Button color='primary' className='acc' onClick={() => this.actionRight('swipe right', item.name, item.uid, item.profileUrl)}> <img src='https://www.healthandsafetysigns.co.uk/wp-content/uploads/2017/08/tick.png' width='55px' height='40px' /></Button>
+                                                    <Button color='primary' className='Rej' onClick={() => this.actionLeft('swipe left', index)}> <img src='http://www.urltarget.com/images/cancel-delete-cross-check-box-check-tick-icon.png' width='55px' height='40px' /></Button>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </Card>
+                                    </Card>
+                                )
+                            }
                             )}
                         </Cards>
 
@@ -277,11 +418,29 @@ class Dashboard extends Component {
 
                     }
                 </center>
-                { !calender && location && this.renderLocations()}
+                {!calender && location && this.renderLocations()}
                 {calender && this.renderCalender()}
+                {locationRender && this.renderDirection()}
+                {!calender && !setMet && <PropReq/>}
             </div>
         )
     }
 }
 
 export default Dashboard;
+
+
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+        defaultZoom={8}
+        center={{ lat: props.cord.lat, lng: props.cord.lng }}
+    >
+        <Marker position='center' defaultDraggable={false} onDrag={props.new} position={{ lat: props.cord.lat, lng: props.cord.lng }} />
+        <Marker position='center' defaultDraggable={false} onDrag={props.new} position={{ lat: props.destination.latitude, lng: props.destination.loongitude }} />
+
+        {props.directions && <DirectionsRenderer directions={props.directions} />}
+
+
+    </GoogleMap>
+))
