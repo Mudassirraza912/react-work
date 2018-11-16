1854a38b495181cdde6e9d6918cd2fdf9761ed97
Mudassirraza912 <39431881+Mudassirraza912@users.noreply.github.com>
2018-11-16 15:25:56 +0500

    Add files via upload

---
+++ MeetUp App Task/src/components/index.js
@@ -0,0 +1,14 @@
+import App from '../App'
+import Dashboard from './dashboard/dashboard'
+import PropReq from './dashboard/propsal&Req'
+import Map from './profile Screen/map'
+import Setup from './profile Screen/setupprofile'
+import Profile from './dashboard/profile'
+export {
+    App,
+    Dashboard,
+    PropReq,
+    Map,
+    Setup,
+    Profile
+}
